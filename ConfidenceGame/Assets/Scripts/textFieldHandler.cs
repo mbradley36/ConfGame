48a94533cd4abdfe596c87c132cb2e95d785434d
Dom <domvalles@gmail.com>
2015-04-22 18:18:18 -0700

    Added an input field that starts selected and passes the string after pressing enter, which also clears it.

---
+++ ConfidenceGame/Assets/Scripts/textFieldHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class textFieldHandler : MonoBehaviour {
+
+	public InputField sentence = null;
+	
+	// Use this for initialization
+	void Start () {
+		//setup for the fuction that will return the input field string
+		InputField.SubmitEvent submitEvent = new InputField.SubmitEvent ();
+		submitEvent.AddListener (saveString);
+		sentence.onEndEdit = submitEvent;
+		sentence.Select ();
+		sentence.ActivateInputField ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	//function that is called when enter is pressed after typing something into input field sentence
+	//the string it takes in is what the user typed
+	void saveString(string typed){
+		Debug.Log (typed);
+		sentence.Select ();
+		sentence.ActivateInputField ();
+	}
+}
