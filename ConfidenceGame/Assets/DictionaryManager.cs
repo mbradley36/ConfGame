768b2a85b6d4ef5dc6a3dc5d8486ede4313fce95
mirandajbradley@gmail.com <Plutomjb1>
2015-04-27 21:28:09 -0400

    negative dictionary

--- ConfidenceGame/Assets/DictionaryManager.cs
+++ ConfidenceGame/Assets/DictionaryManager.cs
@@ -8,6 +8,7 @@ public class DictionaryManager : MonoBehaviour {
 	public static DictionaryManager instance{get; private set;}
 	private Dictionary<string, int> positiveDictionary = new Dictionary<string, int>();
 	private Dictionary<string, int> multiplierDictionary = new Dictionary<string, int>();
+	private Dictionary<string, int> negativeDictionary = new Dictionary<string, int>();
 	public int startingPointVal = 4;
 
 	void Awake(){
@@ -18,8 +19,10 @@ public class DictionaryManager : MonoBehaviour {
 	void Start () {
 		string file = Application.dataPath + "/Resources/positiveWordList.txt";
 		string file2 = Application.dataPath + "/Resources/multiplierList.txt";
+		string file3 = Application.dataPath + "/Resources/negativeWordList.txt";
 		FillDictionary (file, positiveDictionary);
 		FillDictionary (file2, multiplierDictionary);
+		FillDictionary (file3, negativeDictionary);
 	}
 
 	void FillDictionary(string file, Dictionary<string, int> d){
@@ -49,21 +52,26 @@ public class DictionaryManager : MonoBehaviour {
 	
 	public int CheckDictionary(string sentence){
 		string[] words = sentence.Split (' ');
+		bool negative = false;
 		//Debug.Log (words.Length);
 		ArrayList multiples = new ArrayList ();
 		int points = 0;
 		if(words.Length > 1) {
 			for(int i = 0; i < words.Length; i++) {
+				if(negativeDictionary.ContainsKey(words[i])) {
+					return 0;
+				}
 				if(positiveDictionary.ContainsKey(words[i])) {
 					int worth = positiveDictionary[words[i]];
 					points += worth;
 					if(worth > 0) positiveDictionary[words[i]] = worth-1;
 				}
-				if(multiplierDictionary.ContainsKey(words[i]) {
+				if(multiplierDictionary.ContainsKey(words[i])) {
 					multiples.Add(startingPointVal);
 				}
 			}
 		}
+		if(words.Length > 7) multiples.Add(startingPointVal);
 		foreach(int i in multiples) {
 					points *= i;
 		}
