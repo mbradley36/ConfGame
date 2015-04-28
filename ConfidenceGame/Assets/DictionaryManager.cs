76b32a9ffe268e20b7b931b4a1020b02736399bc
mirandajbradley@gmail.com <Plutomjb1>
2015-04-27 21:20:46 -0400

    multiplier dictionary

--- ConfidenceGame/Assets/DictionaryManager.cs
+++ ConfidenceGame/Assets/DictionaryManager.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 public class DictionaryManager : MonoBehaviour {
 	public static DictionaryManager instance{get; private set;}
 	private Dictionary<string, int> positiveDictionary = new Dictionary<string, int>();
-
+	private Dictionary<string, int> multiplierDictionary = new Dictionary<string, int>();
 	public int startingPointVal = 4;
 
 	void Awake(){
@@ -17,11 +17,16 @@ public class DictionaryManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		string file = Application.dataPath + "/Resources/positiveWordList.txt";
-		Debug.Log (file);
+		string file2 = Application.dataPath + "/Resources/multiplierList.txt";
+		FillDictionary (file, positiveDictionary);
+		FillDictionary (file2, multiplierDictionary);
+	}
+
+	void FillDictionary(string file, Dictionary<string, int> d){
 		try {
 			string line;
 			StreamReader reader = new StreamReader(file, Encoding.Default);
-
+			
 			using(reader){
 				do {
 					line = reader.ReadLine ();
@@ -29,23 +34,23 @@ public class DictionaryManager : MonoBehaviour {
 						string[] positiveWord = line.Split(' ');
 						if(positiveWord.Length > 0) {
 							for(int i = 0; i < positiveWord.Length; i++){
-								positiveDictionary.Add(positiveWord[i], startingPointVal);
+								d.Add(positiveWord[i], startingPointVal);
 							}
 						}
 					}
 				} while (line != null);
 			}
-
+			
 			reader.Close();
 		} catch (IOException e) {
 			Debug.Log ("couldn't read positive word file");
 		}
-	
 	}
 	
 	public int CheckDictionary(string sentence){
 		string[] words = sentence.Split (' ');
 		//Debug.Log (words.Length);
+		ArrayList multiples = new ArrayList ();
 		int points = 0;
 		if(words.Length > 1) {
 			for(int i = 0; i < words.Length; i++) {
@@ -54,9 +59,15 @@ public class DictionaryManager : MonoBehaviour {
 					points += worth;
 					if(worth > 0) positiveDictionary[words[i]] = worth-1;
 				}
+				if(multiplierDictionary.ContainsKey(words[i]) {
+					multiples.Add(startingPointVal);
+				}
 			}
-			Debug.Log ("points worth: " + points);
 		}
+		foreach(int i in multiples) {
+					points *= i;
+		}
+		//Debug.Log ("points worth: " + points);
 		return points;
 	}
 }
